bc86f479ef2e20142f5da5eeb7e102b7d3b5641b
richcollector <ktw9115@naver.com>
2023-08-30 11:16:20 +0900

    style: react-markdown 적용

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,9 +1,9 @@
-import styles from '../../utils/styles/Error.module.scss';
+import styles from '../../utils/styles/Etc.module.scss';
 
 function ErrorPage() {
 	return (
 		<>
-			<div className={styles.errorBox}>
+			<div className={styles.wrapper}>
 				<h1>관리자에게 문의해 주세요.</h1>
 			</div>
 		</>
